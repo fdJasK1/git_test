e00d05f8071d97820b77b0703d27d71c7d26419c
fdJasK1 <fdjaskl0722@gmail.com>
2025-02-04 12:44:20 +0800

    Feature: add get Function

--- Dispatcher.java
+++ Dispatcher.java
@@ -2,4 +2,16 @@ public class Dispatcher {
     int n_threads;
     int thread_count;
     int wait_time;
+
+    public int get_thread_count() {
+        return thread_count;
+    }
+
+    public int get_n_threads() {
+        return n_threads;
+    }
+
+    public int get_wait_time() {
+        return n_threads;
+    }
 }
